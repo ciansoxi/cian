30bc6ef4f204337a8c7b257b26343922e50db9d3
ciansoxi <83388029+ciansoxi@users.noreply.github.com>
2021-04-29 13:07:04 +0800

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6066e41cf11f97001e814fea",
+				"6066e41cf11f97001e814fec"
+			]
+		},
+		"6066e41cf11f97001e814fea": {
+			"id": "6066e41cf11f97001e814fea",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6066e41cf11f97001e814fec": {
+			"id": "6066e41cf11f97001e814fec",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
